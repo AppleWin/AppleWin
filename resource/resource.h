27e3030af3e3b57d8ee8187a848ee3f28527fc53
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2006-02-28 18:40:28 +0000

    Added Drive Protection Light

--- resource/resource.h
+++ resource/resource.h
@@ -2,31 +2,32 @@
 // Microsoft Visual C++ generated include file.
 // Used by Applewin.rc
 //
+#define ID_MENU_DISK_POPUP              4
 #define IDC_COMPUTER                    101
 #define IDC_JOYSTICK0                   102
 #define IDC_SOUNDTYPE                   103
 #define IDC_SERIALPORT                  104
-#define IDR_MENU1                       104
 #define IDR_ACCELERATOR1                105
-#define IDC_VIDEOTYPE                   105
-#define IDD_PROPPAGE_INPUT              106
-#define IDC_AUTHENTIC_SPEED             106
-#define IDD_PROPPAGE_CONFIG             107
-#define IDC_CUSTOM_SPEED                107
-#define IDC_SLIDER_CPU_SPEED            108
-#define IDD_PROPPAGE_SOUND              108
-#define IDC_DISKTYPE                    109
-#define IDD_PROPPAGE_SAVESTATE          109
-#define IDC_JOYSTICK1                   110
-#define IDD_PROPPAGE_DISK               110
-#define IDC_BENCHMARK                   111
-#define IDC_LOADSTATE                   112
-#define IDR_HDDRVR                      115
-#define IDC_0_5_MHz                     121
+#define IDC_VIDEOTYPE                   106
+#define IDD_PROPPAGE_INPUT              107
+#define IDC_AUTHENTIC_SPEED             108
+#define IDD_PROPPAGE_CONFIG             109
+#define IDC_CUSTOM_SPEED                110
+#define IDC_SLIDER_CPU_SPEED            111
+#define IDD_PROPPAGE_SOUND              112
+#define IDC_DISKTYPE                    113
+#define IDD_PROPPAGE_SAVESTATE          114
+#define IDC_JOYSTICK1                   115
+#define IDD_PROPPAGE_DISK               116
+#define IDC_BENCHMARK                   117
+#define IDC_LOADSTATE                   118
+#define IDR_HDDRVR                      119
+#define IDC_0_5_MHz                     120
 #define IDB_APPLEWIN                    121
 #define IDC_1_0_MHz                     122
 #define IDC_2_0_MHz                     123
 #define IDC_MAX_MHz                     124
+#define IDR_MENU                        125
 #define IDC_MB_ENABLE                   130
 #define IDC_KEYB_BUFFER_ENABLE          1005
 #define IDC_SAVESTATE                   1006
@@ -53,14 +54,17 @@
 #define IDM_EXIT                        40001
 #define IDM_HELP                        40002
 #define IDM_ABOUT                       40003
+#define ID_DISKMENU_EJECT               40004
+#define ID_DISKMENU_WRITEPROTECTION_ON  40005
+#define ID_DISKMENU_WRITEPROTECTION_OFF 40006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        122
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         40007
 #define _APS_NEXT_CONTROL_VALUE         1031
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
