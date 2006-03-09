bdf8dfdfe790aee9e85fed640162009721e81342
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2006-03-09 21:40:16 +0000

    Cleaned up ImageOpen()

--- AppleWin/source/DiskImage.h
+++ AppleWin/source/DiskImage.h
@@ -8,6 +8,17 @@ BOOL    ImageBoot (HIMAGE);
 void    ImageClose (HIMAGE);
 void    ImageDestroy ();
 void    ImageInitialize ();
-int     ImageOpen (LPCTSTR,HIMAGE *,BOOL *,BOOL);
+
+enum ImageError_e
+{
+	IMAGE_ERROR_BAD_POINTER    =-1,
+	IMAGE_ERROR_NONE           = 0,
+	IMAGE_ERROR_UNABLE_TO_OPEN = 1,
+	IMAGE_ERROR_BAD_SIZE       = 2
+};
+
+//int     ImageOpen (LPCTSTR,HIMAGE *,BOOL *,BOOL);
+int ImageOpen (LPCTSTR imagefilename, HIMAGE *hDiskImage_, BOOL *pWriteProtected_, BOOL bCreateIfNecessary );
+
 void    ImageReadTrack (HIMAGE,int,int,LPBYTE,int *);
 void    ImageWriteTrack (HIMAGE,int,int,LPBYTE,int);
