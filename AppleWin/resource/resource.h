490afef5eaaadfd882e927aa951e646b6dc0be36
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2006-07-02 09:55:35 +0000

    Added new Apple Debugger Font

--- AppleWin/resource/resource.h
+++ AppleWin/resource/resource.h
@@ -79,7 +79,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         40007
 #define _APS_NEXT_CONTROL_VALUE         1043
 #define _APS_NEXT_SYMED_VALUE           101
