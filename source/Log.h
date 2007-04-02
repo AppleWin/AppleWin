9794ccbdbe72af5375d6b8e4ed8d985a33e5e748
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2007-04-02 14:32:50 +0000

    Changed __VA_ARGS__ to work on compilers that don't support it

--- source/Log.h
+++ source/Log.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #if _DEBUG
-    #define LOG(format, ...) LogOutput(format, __VA_ARGS__)
+// __VA_ARGS__ not supported on MSVC++ .NET 7.x
+//    #define LOG(format, ...) LogOutput(format, __VA_ARGS__)
+    #define LOG(format, params) LogOutput(format, params)
 #else
-    #define LOG(...)
+//    #define LOG(...)
+    #define LOG(format, params)
 #endif
 
 extern void LogOutput(LPCTSTR format, ...);
