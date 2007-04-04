3bfac2b9338dcefb084651f812154501f76201ee
tomch <tomch@b5e51362-a710-0410-a134-be2176fc2e42>
2007-04-04 21:28:10 +0000

    Added preprocessor define _VC71 to VC7.1 project & exclude LOG'ing for VC7.1 builds (only affects debug builds)

--- source/Log.h
+++ source/Log.h
@@ -1,12 +1,11 @@
 #pragma once
 
-#if _DEBUG
-// __VA_ARGS__ not supported on MSVC++ .NET 7.x
-//    #define LOG(format, ...) LogOutput(format, __VA_ARGS__)
-    #define LOG(format, params) LogOutput(format, params)
-#else
-//    #define LOG(...)
-    #define LOG(format, params)
+#ifndef _VC71	// __VA_ARGS__ not supported on MSVC++ .NET 7.x
+	#ifdef _DEBUG
+		#define LOG(format, ...) LogOutput(format, __VA_ARGS__)
+	#else
+		#define LOG(...)
+	#endif
 #endif
 
 extern void LogOutput(LPCTSTR format, ...);
