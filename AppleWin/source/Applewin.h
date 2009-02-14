9eb2f444c3b505dd74d08890617e4ae02bfc7fb6
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2009-02-14 03:54:12 +0000

    Clock cleanup

--- AppleWin/source/Applewin.h
+++ AppleWin/source/Applewin.h
@@ -42,7 +42,13 @@ extern DWORD      g_dwCyclesThisFrame;
 extern FILE*      g_fh;				// Filehandle for log file
 extern bool       g_bDisableDirectSound;	// Cmd line switch: don't init DS (so no MB support)
 
-extern UINT		g_Slot4;	// Mockingboard or Mouse in slot4
+// TODO: Make g_CurrentPeripherals[MAX_SLOTS] = { CT_Empty }
+extern UINT g_Slot4;	// Mockingboard or Mouse in slot4
+
+// TODO: Move into: g_CurrentPeripherals[]
+extern UINT g_uMouseInSlot4;
+//extern UINT g_uClockInSlot4;
+extern UINT g_uZ80InSlot5;
 
 extern HANDLE	g_hCustomRomF8;		// NULL if no custom rom
 
