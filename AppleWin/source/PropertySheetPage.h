261b09665e94d59460bf272eae51f01d31ccefb3
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2009-02-14 03:36:54 +0000

    pretty print - misc clean up

--- AppleWin/source/PropertySheetPage.h
+++ AppleWin/source/PropertySheetPage.h
@@ -1,20 +1,24 @@
 #pragma once
 
-void    PSP_Init();
-DWORD   PSP_GetVolumeMax();
-bool    PSP_SaveStateSelectImage(HWND hWindow, bool bSave);
-void ui_tfe_settings_dialog(HWND hwnd);
-void * get_tfe_interface(void);
-void get_tfe_enabled(int *tfe_enabled);
-string BrowseToFile(HWND hWindow, TCHAR* pszTitle, TCHAR* REGVALUE, TCHAR* FILEMASKS);
+// Globals
+	extern UINT g_uScrollLockToggle;
+	extern UINT g_uMouseShowCrosshair;
+	extern UINT g_uMouseRestrictToWindow;
+	extern UINT g_uTheFreezesF8Rom;
+	extern DWORD g_uCloneType;
+	extern HWND hwConfigTab;
+	extern HWND hwAdvancedTab;
 
-extern UINT g_uScrollLockToggle;
-extern UINT g_uMouseInSlot4;
-extern UINT g_uMouseShowCrosshair;
-extern UINT g_uMouseRestrictToWindow;
-extern UINT g_uTheFreezesF8Rom;
-extern DWORD g_uCloneType;
-extern HWND hwConfigTab;
-extern HWND hwAdvancedTab;
 
-extern UINT g_uZ80InSlot5;
+
+// Prototypes
+	void    PSP_Init();
+	DWORD   PSP_GetVolumeMax();
+	bool    PSP_SaveStateSelectImage(HWND hWindow, bool bSave);
+	void ui_tfe_settings_dialog(HWND hwnd);
+	void * get_tfe_interface(void);
+	void get_tfe_enabled(int *tfe_enabled);
+	string BrowseToFile(HWND hWindow, TCHAR* pszTitle, TCHAR* REGVALUE, TCHAR* FILEMASKS);
+
+	void Config_Save_Video();
+	void Config_Load_Video();
