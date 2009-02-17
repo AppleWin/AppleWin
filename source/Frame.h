6b5aea9a4f343fbb99092fbb8dd711f682d60ea8
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2009-02-17 02:12:39 +0000

    CtrlReset cleanup Ctrl-F2 = CtrlReset

--- source/Frame.h
+++ source/Frame.h
@@ -1,42 +1,48 @@
 #pragma once
 
+// Keyboard -- keystroke type
 enum {NOT_ASCII=0, ASCII};
 
 // 3D Border
-#define  VIEWPORTX   5
-#define  VIEWPORTY   5
+	#define  VIEWPORTX   5
+	#define  VIEWPORTY   5
 
-// 560 = Double Hi-Res
-// 384 = Doule Scan Line
-#define  FRAMEBUFFER_W  560
-#define  FRAMEBUFFER_H  384
+	// 560 = Double Hi-Res
+	// 384 = Doule Scan Line
+	#define  FRAMEBUFFER_W  560
+	#define  FRAMEBUFFER_H  384
 
 // Direct Draw -- For Full Screen
-extern	LPDIRECTDRAW        g_pDD;
-extern	LPDIRECTDRAWSURFACE g_pDDPrimarySurface;
-extern	IDirectDrawPalette* g_pDDPal;
+	extern	LPDIRECTDRAW        g_pDD;
+	extern	LPDIRECTDRAWSURFACE g_pDDPrimarySurface;
+	extern	IDirectDrawPalette* g_pDDPal;
 
 // Win32
-extern HWND       g_hFrameWindow;
-extern HDC        g_hFrameDC;
-extern BOOL       g_bIsFullScreen;
+	extern HWND       g_hFrameWindow;
+	extern HDC        g_hFrameDC;
+	extern BOOL       g_bIsFullScreen;
 
-void    FrameCreateWindow ();
-HDC     FrameGetDC ();
-HDC     FrameGetVideoDC (LPBYTE *,LONG *);
-void    FrameRefreshStatus (int);
-void    FrameRegisterClass ();
-void    FrameReleaseDC ();
-void    FrameReleaseVideoDC ();
-void	FrameSetCursorPosByMousePos();
+// Emulator
+	extern bool   g_bFreshReset;
+	extern string PathFilename[2];
+	extern bool   g_bScrollLock_FullSpeed;
+	extern int    g_nCharsetType;
 
-extern  string PathFilename[2];
+// Prototypes
+	void CtrlReset();
 
-LRESULT CALLBACK FrameWndProc (
-	HWND   window,
-	UINT   message,
-	WPARAM wparam,
-	LPARAM lparam );
+	void    FrameCreateWindow ();
+	HDC     FrameGetDC ();
+	HDC     FrameGetVideoDC (LPBYTE *,LONG *);
+	void    FrameRefreshStatus (int);
+	void    FrameRegisterClass ();
+	void    FrameReleaseDC ();
+	void    FrameReleaseVideoDC ();
+	void	FrameSetCursorPosByMousePos();
 
-extern bool g_bScrollLock_FullSpeed;
-extern int g_nCharsetType;
+	LRESULT CALLBACK FrameWndProc (
+		HWND   window,
+		UINT   message,
+		WPARAM wparam,
+		LPARAM lparam );
+
