f997ff0dfec39139df35d7ba188c2942b2af5b69
tomch <tomch@b5e51362-a710-0410-a134-be2176fc2e42>
2009-05-27 21:38:46 +0000

    Merge from AppleWin-Tom @ r563 . Removed $Id from comment header

--- AppleWin/source/AY8910.cpp
+++ AppleWin/source/AY8910.cpp
@@ -2,8 +2,6 @@
    Copyright (c) 2000-2007 Russell Marks, Matan Ziv-Av, Philip Kendall,
                            Fredrick Meunier
 
-   $Id$
-
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
