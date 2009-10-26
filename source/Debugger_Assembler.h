e270da67968fb6445b762e6bdd931d2930ef8b76
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2009-10-26 01:22:51 +0000

    2.6.2.2 cleaned up assembler pseudo opcodes

--- source/Debugger_Assembler.h
+++ source/Debugger_Assembler.h
@@ -1,6 +1,117 @@
 #ifndef DEBUGGER_ASSEMBLER_H
 #define DEBUGGER_ASSEMBLER_H
 
+// Directives
+
+	// Assemblers
+	//     A = Acme
+	//     B = Big Mac            S= S-C Macro Assembler
+	//     K = DOS Tool Kit       T = TED II
+	//     L = Lisa               W = Weller's Assembler
+	//     M = Merlin
+	//     u = MicroSparc
+	//     O = ORCA/M
+	enum AsmAcmeDirective_e
+	{
+		 ASM_A_DEFINE_BYTE
+	,NUM_ASM_A_DIRECTIVES
+	};
+
+	enum AsmBigMacDirective_e
+	{
+		 ASM_B_DEFINE_BYTE
+	,NUM_ASM_B_DIRECTIVES
+	};
+
+	enum AsmDosToolKitDirective_e
+	{
+		 ASM_K_DEFINE_BYTE
+	,NUM_ASM_K_DIRECTIVES
+	};
+
+	enum AsmLisaDirective_e
+	{
+		 ASM_L_DEFINE_BYTE
+	,NUM_ASM_L_DIRECTIVES
+	};
+
+	enum AsmMerlinDirective_e
+	{
+		  ASM_M_ASCII
+		, ASM_M_DEFINE_WORD
+		, ASM_M_DEFINE_BYTE
+		, ASM_M_DEFINE_STORAGE
+		, ASM_M_HEX
+		, ASM_M_ORIGIN
+	, NUM_ASM_M_DIRECTIVES
+		, ASM_M_DEFINE_BYTE_ALIAS
+		, ASM_M_DEFINE_WORD_ALIAS
+	};
+
+	enum AsmMicroSparcDirective_e
+	{
+		 ASM_u_DEFINE_BYTE
+	,NUM_ASM_u_DIRECTIVES
+	};
+
+	enum AsmOrcamDirective_e
+	{
+		 ASM_O_DEFINE_BYTE
+	,NUM_ASM_O_DIRECTIVES
+	};
+
+	enum AsmSCMacroDirective_e
+	{
+		 ASM_S_ORIGIN
+		,ASM_S_TARGET_ADDRESS
+		,ASM_S_END_PROGRAM
+		,ASM_S_EQUATE
+		,ASM_S_DATA
+		,ASM_S_ASCII_STRING
+		,ASM_S_HEX_STRING
+	,NUM_ASM_S_DIRECTIVES
+	};
+
+	enum AsmTedDirective_e
+	{
+		 ASM_T_DEFINE_BYTE
+	,NUM_ASM_T_DIRECTIVES
+	};
+
+	enum AsmWellersDirective_e
+	{
+		 ASM_W_DEFINE_BYTE
+	,NUM_ASM_W_DIRECTIVES
+	};
+
+	// NOTE: Must keep in sync: AsmDirectives_e g_aAssemblerDirectives
+	enum AsmDirectives_e
+	{
+		FIRST_A_DIRECTIVE = 1,
+		FIRST_B_DIRECTIVE = FIRST_A_DIRECTIVE + NUM_ASM_A_DIRECTIVES, // Acme
+		FIRST_K_DIRECTIVE = FIRST_B_DIRECTIVE + NUM_ASM_B_DIRECTIVES, // Big Mac
+		FIRST_L_DIRECTIVE = FIRST_K_DIRECTIVE + NUM_ASM_K_DIRECTIVES, // DOS Tool Kit
+		FIRST_M_DIRECTIVE = FIRST_L_DIRECTIVE + NUM_ASM_L_DIRECTIVES, // Lisa
+		FIRST_u_DIRECTIVE = FIRST_M_DIRECTIVE + NUM_ASM_M_DIRECTIVES, // Merlin
+		FIRST_O_DIRECTIVE = FIRST_u_DIRECTIVE + NUM_ASM_u_DIRECTIVES, // MicroSparc
+		FIRST_S_DIRECTIVE = FIRST_O_DIRECTIVE + NUM_ASM_O_DIRECTIVES, // Orca
+		FIRST_T_DIRECTIVE = FIRST_S_DIRECTIVE + NUM_ASM_S_DIRECTIVES, // SC
+		FIRST_W_DIRECTIVE = FIRST_T_DIRECTIVE + NUM_ASM_T_DIRECTIVES, // Ted
+		NUM_ASM_DIRECTIVES= FIRST_W_DIRECTIVE + NUM_ASM_W_DIRECTIVES, // Ted
+
+//		NUM_ASM_DIRECTIVES =  1 +  // Opcode ... rest are psuedo opcodes
+//			NUM_ASM_A_DIRECTIVES + // Acme
+//			NUM_ASM_B_DIRECTIVES + // Big Mac
+//			NUM_ASM_K_DIRECTIVES + // DOS Tool Kit
+//			NUM_ASM_L_DIRECTIVES + // Lisa
+//			NUM_ASM_M_DIRECTIVES + // Merlin
+//			NUM_ASM_u_DIRECTIVES + // MicroSparc
+//			NUM_ASM_O_DIRECTIVES + // Orca
+//			NUM_ASM_S_DIRECTIVES + // SC
+//			NUM_ASM_T_DIRECTIVES + // Ted
+//			NUM_ASM_W_DIRECTIVES   // Weller
+	};
+
 // Addressing _____________________________________________________________________________________
 
 	extern AddressingMode_t g_aOpmodes[ NUM_ADDRESSING_MODES ];
@@ -25,7 +136,7 @@
 
 	struct AssemblerDirective_t
 	{
-		char   m_sMnemonic[ MAX_MNEMONIC_LEN + 1 ];	
+		char  *m_pMnemonic;
 		Hash_t m_nHash;
 	};
 
@@ -38,6 +149,8 @@
 
 	extern const Opcodes_t g_aOpcodes65C02[ NUM_OPCODES ];
 	extern const Opcodes_t g_aOpcodes6502 [ NUM_OPCODES ];
+
+	extern AssemblerDirective_t g_aAssemblerDirectives[ NUM_ASM_DIRECTIVES ];
 
 // Prototypes _______________________________________________________________
 
