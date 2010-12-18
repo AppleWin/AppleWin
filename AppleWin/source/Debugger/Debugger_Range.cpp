f9c9f02b82b27b81a1732fba15542bdca5c29deb
mpohoreski <mpohoreski@b5e51362-a710-0410-a134-be2176fc2e42>
2010-12-18 14:54:04 +0000

    2.6.2.21 Fixed: Missing GPL / copyright on some debugger files, updated year.

--- AppleWin/source/Debugger/Debugger_Range.cpp
+++ AppleWin/source/Debugger/Debugger_Range.cpp
@@ -4,7 +4,7 @@ AppleWin : An Apple //e emulator for Windows
 Copyright (C) 1994-1996, Michael O'Brien
 Copyright (C) 1999-2001, Oliver Schmidt
 Copyright (C) 2002-2005, Tom Charlesworth
-Copyright (C) 2006-2009, Tom Charlesworth, Michael Pohoreski
+Copyright (C) 2006-2010, Tom Charlesworth, Michael Pohoreski
 
 AppleWin is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /* Description: Debugger Utility Range
  *
- * Author: Copyright (C) 2006-2009 Michael Pohoreski
+ * Author: Copyright (C) 2006-2010 Michael Pohoreski
  */
 
 #include "StdAfx.h"
