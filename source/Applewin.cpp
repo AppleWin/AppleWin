ae762762e716e093575ee7cb89047a2873723d36
michaelangel007 <testcraft@sharedcraft.com>
2014-08-28 10:43:32 -0700

    #206 Based on FaceBook poll, changed memory initialization pattern to: MIP_FF_00_FULL_PAGE

--- source/Applewin.cpp
+++ source/Applewin.cpp
@@ -96,7 +96,7 @@ FILE*		g_fh			= NULL;
 bool		g_bDisableDirectInput = false;
 bool		g_bDisableDirectSound = false;
 bool		g_bDisableDirectSoundMockingboard = false;
-int			g_nMemoryClearType = MIP_FF_00_FULL_PAGE; // Note: -1 = random
+int			g_nMemoryClearType = MIP_FF_00_FULL_PAGE; // Note: -1 = random in Memory.cpp MemReset()
 
 IPropertySheet&		sg_PropertySheet = * new CPropertySheet;
 CSuperSerialCard	sg_SSC;
