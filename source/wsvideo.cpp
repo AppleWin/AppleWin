123a278786d13589ce360690d259956f90ab70c6
michaelangel007 <testcraft@sharedcraft.com>
2014-12-18 12:37:34 -0800

    added precompiled header include

--- source/wsvideo.cpp
+++ source/wsvideo.cpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with AppleWin; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
+#include "StdAfx.h"
 #include "cs.h"
 #include "wsvideo.h"
 #include <math.h>
