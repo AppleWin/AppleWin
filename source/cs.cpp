423369d845ec0dfd0934d45a2385b0986e01a6e4
michaelangel007 <testcraft@sharedcraft.com>
2014-12-18 12:37:05 -0800

    Fix inconsistent CR/LF line endings; added precompiled header include

--- source/cs.cpp
+++ source/cs.cpp
@@ -18,6 +18,7 @@ along with AppleWin; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#include "StdAfx.h"
 #include "cs.h"
 
 unsigned char csbits[2][256][8];
