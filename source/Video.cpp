AppleWin/AppleWin

commit 09fa6ccef54ea2479488853377dc3826e32e9d98

Fixed #342

source/Video.cpp
@@ -1168,9 +1168,10 @@ void VideoRefreshScreen ( int bVideoModeFlags, UINT uDelayRefresh /* =0 */ )
 	DebugRefresh(0);
 #endif
 
-	if( bVideoModeFlags )
+	if (g_nAppMode == MODE_DEBUG || g_nAppMode == MODE_PAUSED)
 	{
-		NTSC_SetVideoMode( bVideoModeFlags );
+		if (g_nAppMode == MODE_DEBUG)
+			NTSC_SetVideoMode( bVideoModeFlags );
 		NTSC_VideoUpdateCycles( VIDEO_SCANNER_6502_CYCLES );
 	}
 

source/NTSC.cpp
@@ -1481,7 +1481,7 @@ void NTSC_SetVideoMode( int bVideoModeFlags )
 	g_nTextPage  = 1;
 	g_nHiresPage = 1;
 	if (bVideoModeFlags & VF_PAGE2) {
-		// Apple IIe, Techical Nodtes, #3: Double High-Resolution Graphics
+		// Apple IIe, Technical Notes, #3: Double High-Resolution Graphics
 		// 80STORE must be OFF to display page 2
 		if (0 == (bVideoModeFlags & VF_80STORE)) {
 			g_nTextPage  = 2;

commit f559deddc36f9713dcd0ebb49b552bf3475add72

Fix for flickery/bad video updates when MODE_RUNNING and full-speed (broken in previous 09fa6cc)

source/Video.cpp
@@ -1168,9 +1168,12 @@ void VideoRefreshScreen ( int bVideoModeFlags, UINT uDelayRefresh /* =0 */ )
 	DebugRefresh(0);
 #endif
 
-	if (g_nAppMode == MODE_DEBUG || g_nAppMode == MODE_PAUSED)
+	if (bVideoModeFlags || g_nAppMode == MODE_PAUSED)
 	{
-		if (g_nAppMode == MODE_DEBUG)
+		// bVideoModeFlags set if:
+		// . MODE_DEBUG   : always
+		// . MODE_RUNNING : called from VideoRedrawScreen(), eg. during full-speed
+		if (bVideoModeFlags)
 			NTSC_SetVideoMode( bVideoModeFlags );
 		NTSC_VideoUpdateCycles( VIDEO_SCANNER_6502_CYCLES );
 	}