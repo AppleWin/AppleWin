ce0ced0c44ad2c65960a1843604442bb70dd9b36
Andrea Odetti <mariofutire@gmail.com>
2017-05-16 09:06:16 +0100

    Use various replacements in StdAfx.h
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- source/StdAfx.h
+++ source/StdAfx.h
@@ -15,29 +15,13 @@
 #endif
 
 // Not needed in VC7.1, but needed in VC Express
-#include <tchar.h>
-
-#include <crtdbg.h>
-#include <dsound.h>
-#include <dshow.h>
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#if _MSC_VER >= 1600	// <stdint.h> supported from VS2010 (cl.exe v16.00)
 #include <stdint.h> // cleanup WORD DWORD -> uint16_t uint32_t
-#else
-typedef UINT8 uint8_t;
-typedef UINT16 uint16_t;
-typedef UINT32 uint32_t;
-#endif
 
-#include <windows.h>
-#include <winuser.h> // WM_MOUSEWHEEL
-#include <commctrl.h>
-#include <ddraw.h>
-#include <htmlhelp.h>
 #include <assert.h>
 
 #include <algorithm>
@@ -48,10 +32,12 @@ typedef UINT32 uint32_t;
 #include <vector>
 #include <memory>
 
+#include <linux/wwrapper.h>
+
 // SM_CXPADDEDBORDER is not supported on 2000 & XP:
 // http://msdn.microsoft.com/en-nz/library/windows/desktop/ms724385(v=vs.85).aspx
 #ifndef SM_CXPADDEDBORDER
 #define SM_CXPADDEDBORDER 92
 #endif
 
-#define USE_SPEECH_API
+//#define USE_SPEECH_API
