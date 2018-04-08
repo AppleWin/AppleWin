29008161678cdd429cb464fa9b76c99bedfe643e
tomcw <tomcw@users.noreply.github.com>
2018-04-08 17:48:08 +0100

    Removed duplicate extern FILE* g_fh

--- source/Speaker.cpp
+++ source/Speaker.cpp
@@ -31,6 +31,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Applewin.h"
 #include "CPU.h"
 #include "Frame.h"
+#include "Log.h"
 #include "Memory.h"
 #include "SoundCore.h"
 #include "Speaker.h"
