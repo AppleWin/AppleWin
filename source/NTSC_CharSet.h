875a0bdd1181b414425cb6b18cb2bd0ff90231b0
tomcw <tomcw@users.noreply.github.com>
2019-01-10 22:00:53 +0000

    Very minor clean-up

--- source/NTSC_CharSet.h
+++ source/NTSC_CharSet.h
@@ -3,7 +3,6 @@
 typedef unsigned char (*csbits_t)[256][8];
 
 extern unsigned char csbits_enhanced2e[2][256][8];	// Enhanced //e (2732 4K video ROM)
-extern unsigned char csbits_2e[2][256][8];			// Original //e (no mousetext)
 extern unsigned char csbits_a2[1][256][8];			// ][ and ][+
 extern unsigned char csbits_pravets82[1][256][8];	// Pravets 82
 extern unsigned char csbits_pravets8M[1][256][8];	// Pravets 8M
