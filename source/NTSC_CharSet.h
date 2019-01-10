4d7a3323e9fa1430b242f79dc072ca38c161ad13
tomcw <tomcw@users.noreply.github.com>
2019-01-10 21:38:21 +0000

    Extended -videorom <file> to also support Original //e (#574)

--- source/NTSC_CharSet.h
+++ source/NTSC_CharSet.h
@@ -10,4 +10,4 @@ extern unsigned char csbits_pravets8M[1][256][8];	// Pravets 8M
 extern unsigned char csbits_pravets8C[2][256][8];	// Pravets 8A & 8C
 
 void make_csbits(void);
-csbits_t GetEnhanced2e_csbits(void);
+csbits_t Get2e_csbits(void);
