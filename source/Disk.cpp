ba801e4e13d236ae0268ff0d2295cfb94c4d0613
Andrea Odetti <mariofutire@gmail.com>
2019-10-12 14:26:52 +0100

    Avoid RAND_MAX overflow.

--- source/Disk.cpp
+++ source/Disk.cpp
@@ -1130,8 +1130,8 @@ void Disk2InterfaceCard::DataLatchReadWOZ(WORD pc, WORD addr, UINT bitCellRemain
 
 		drive.m_headWindow <<= 1;
 		drive.m_headWindow |= (n & floppy.m_bitMask) ? 1 : 0;
-		BYTE outputBit = (drive.m_headWindow & 0xf)	? (drive.m_headWindow >> 1) & 1
-													: (rand() < ((RAND_MAX * 3) / 10)) ? 1 : 0;	// ~30% chance of a 1 bit (Ref: WOZ-2.0)
+		BYTE outputBit = (drive.m_headWindow & 0xf) ? (drive.m_headWindow >> 1) & 1
+		  : (rand() < (RAND_MAX / 10 * 3)) ? 1 : 0;	// ~30% chance of a 1 bit (Ref: WOZ-2.0)
 
 		floppy.m_bitMask >>= 1;
 		if (!floppy.m_bitMask)
