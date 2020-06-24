46f056f38e5c8e85cb032c08d4b1e49948f2895e
Andrea Odetti <mariofutire@gmail.com>
2020-06-24 07:54:04 +0100

    Move duplicate code to their standard location.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- source/linux/duplicates/Mockingboard.cpp
+++ source/linux/duplicates/Mockingboard.cpp
@@ -17,3 +17,39 @@ void    MB_StartOfCpuExecute()
 void MB_SetCumulativeCycles()
 {
 }
+
+void MB_SaveSnapshot(YamlSaveHelper&, unsigned int)
+{
+}
+
+bool MB_LoadSnapshot(class YamlLoadHelper&, UINT, UINT)
+{
+  return true;
+}
+
+void MB_Reset()
+{
+}
+
+void MB_InitializeForLoadingSnapshot()
+{
+}
+
+bool Phasor_LoadSnapshot(class YamlLoadHelper&, UINT, UINT)
+{
+  return true;
+}
+
+void Phasor_SaveSnapshot(YamlSaveHelper&, unsigned int)
+{
+}
+
+std::string MB_GetSnapshotCardName()
+{
+  return "Mockingboard C";
+}
+
+std::string Phasor_GetSnapshotCardName()
+{
+  return "Phasor";
+}
--- source/linux/state.cpp
+++ source/linux/state.cpp
@@ -7,32 +7,17 @@
 
 void SpkrLoadSnapshot(YamlLoadHelper&) { }
 void KeybReset() { }
-void MB_SaveSnapshot(YamlSaveHelper&, unsigned int) { }
 void JoySaveSnapshot(YamlSaveHelper&) { }
 void JoyLoadSnapshot(YamlLoadHelper&) { }
-bool MB_LoadSnapshot(class YamlLoadHelper&, UINT, UINT) { return true; }
 void SetLoadedSaveStateFlag(bool) { }
 void SpkrSaveSnapshot(YamlSaveHelper&) { }
 bool CSuperSerialCard::LoadSnapshot(YamlLoadHelper&, unsigned int, unsigned int) { return true; }
-void MB_Reset() { }
-void MB_InitializeForLoadingSnapshot() { }
-bool Phasor_LoadSnapshot(class YamlLoadHelper&, UINT, UINT) { return true; }
-void Phasor_SaveSnapshot(YamlSaveHelper&, unsigned int) { }
 void IPropertySheet::ApplyNewConfig(CConfigNeedingRestart const&, CConfigNeedingRestart const&) { }
 void FrameUpdateApple2Type() { }
 void CSuperSerialCard::SaveSnapshot(YamlSaveHelper&) { }
 
-std::string MB_GetSnapshotCardName()
-{
-  return "Mockingboard C";
-}
 
 std::string CSuperSerialCard::GetSnapshotCardName()
 {
   return "Super Serial Card";
-}
-
-std::string Phasor_GetSnapshotCardName()
-{
-  return "Phasor";
 }
