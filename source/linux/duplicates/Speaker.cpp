74cab50ceeb2702863d1efb9f07a3ef7f26b87c1
Andrea Odetti <mariofutire@gmail.com>
2020-07-05 13:20:03 +0100

    Remove dependency on std::filesystem and fix a compilation error.

---
+++ source/linux/duplicates/Speaker.cpp
@@ -0,0 +1,16 @@
+#include "StdAfx.h"
+
+#include "YamlHelper.h"
+#include "Common.h"
+
+void SpkrLoadSnapshot(YamlLoadHelper&)
+{
+}
+
+void SpkrSaveSnapshot(YamlSaveHelper&)
+{
+}
+
+void SpkrUpdate_Timer()
+{
+}
