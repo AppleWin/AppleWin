ef913fe827a0395f447e87b211d0badcaf20983e
TomCh <tomcw@users.noreply.github.com>
2020-08-31 10:03:29 +0100

    Removed 65d02 and used the regular 65c02/6502 headers instead (PR #825)
    Removed 65d02.h and reconstructed this code using a combination of existing 6502.h/65c02.h and extra C Pre-Processor macros to include (or omit) the extra heatmap functionality.
    
    We still end up with a normal 65c02 instance and also a debug 65c02 instance, but both will derive from the same 65c02.h file (+ same for the 6502 normal/debug instances).
    
    Also:
    . Added cpu_heatmap.inl for the built-in debugger's read/write operations.
    . Support CpuRead/Write() from Z80 to hook the heatmap r/w.

--- test/TestCPU6502/TestCPU6502.cpp
+++ test/TestCPU6502/TestCPU6502.cpp
@@ -81,8 +81,19 @@ void NTSC_VideoUpdateCycles( long cycles6502 )
 
 #include "../../source/CPU/cpu_general.inl"
 #include "../../source/CPU/cpu_instructions.inl"
+
+#define READ _READ
+#define WRITE(a) _WRITE(a)
+#define HEATMAP_X(pc)
+
 #include "../../source/CPU/cpu6502.h"  // MOS 6502
 #include "../../source/CPU/cpu65C02.h"  // WDC 65C02
+
+#undef READ
+#undef WRITE
+#undef HEATMAP_X
+
+//-------------------------------------
 
 void init(void)
 {
