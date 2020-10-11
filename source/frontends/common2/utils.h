ddea5b139ec8301c6f6e22df324896b53d3797b1
mariofutire@gmail.com <pi@raspberrypi>
2020-10-11 10:30:00 +0100

    Add #pragma once.
    Signed-off-by: mariofutire@gmail.com <pi@raspberrypi>

--- source/frontends/common2/utils.h
+++ source/frontends/common2/utils.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <string>
 #include <linux/windows/wincompat.h>
 
--- source/frontends/qapple/applicationname.h
+++ source/frontends/qapple/applicationname.h
@@ -1,2 +1,4 @@
+#pragma once
+
 #define ORGANIZATION_NAME "AndSoft"
 #define APPLICATION_NAME "QAppleEmulator"
