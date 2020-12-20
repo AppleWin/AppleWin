8d7d2b9647f0fa139444440c924d578f782693a5
tomcw <tomcw@users.noreply.github.com>
2020-12-20 18:44:25 +0000

    VS2019: new debug/release configurations for v141_xp (#693) . NB, the Microsoft SDK 7.1A doesn't contain dxguid.lib & ddraw.lib, so they are linked against these two dx libs that are already part of the AppleWin repo.

--- source/StdAfx.h
+++ source/StdAfx.h
@@ -1,20 +1,3 @@
-//#define WIN32_LEAN_AND_MEAN
-
-// Required for Win98/ME support:
-// . See: http://support.embarcadero.com/article/35754
-// . "GetOpenFileName() fails under Windows 95/98/NT/ME due to incorrect OPENFILENAME structure size"
-#define _WIN32_WINNT 0x0400
-#define WINVER 0x500
-
-// Mouse Wheel is not supported on Win95.
-// If we didn't care about supporting Win95 (compile/run-time errors)
-// we would just define the minimum windows version to support.
-// #define _WIN32_WINDOWS  0x0401
-#ifndef WM_MOUSEWHEEL
-#define WM_MOUSEWHEEL 0x020A
-#endif
-
-// Not needed in VC7.1, but needed in VC Express
 #include <tchar.h>
 
 #include <crtdbg.h>
