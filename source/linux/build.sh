05321679f8e3375b35644a3392ad82e794cc4150
Andrea Odetti <mariofutire@gmail.com>
2020-12-26 19:31:07 +0000

    Clarify what path is required.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- source/linux/build.sh
+++ source/linux/build.sh
@@ -7,5 +7,5 @@ wget https://raw.githubusercontent.com/libretro/RetroArch/master/libretro-common
 mkdir -p build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DLIBRETRO_PATH=libretro-common ..
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DLIBRETRO_COMMON_PATH=libretro-common ..
 make
