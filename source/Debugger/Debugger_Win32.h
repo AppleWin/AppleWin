2672923b00be6922a0dd85d9d26cb5ee6584bfcc
Andrea Odetti <mariofutire@gmail.com>
2021-04-02 18:52:15 +0100

    Debug.cpp: move Win32 functions to separate file.

---
+++ source/Debugger/Debugger_Win32.h
@@ -0,0 +1,33 @@
+/*
+AppleWin : An Apple //e emulator for Windows
+
+Copyright (C) 1994-1996, Michael O'Brien
+Copyright (C) 1999-2001, Oliver Schmidt
+Copyright (C) 2002-2005, Tom Charlesworth
+Copyright (C) 2006-2014, Tom Charlesworth, Michael Pohoreski
+
+AppleWin is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+AppleWin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AppleWin; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* Description: Debugger
+ *
+ * Author: Copyright (C) 2006-2010 Michael Pohoreski
+ */
+
+#pragma once
+
+void FontsDestroy();
+void FontsInitialize();
+void ProcessClipboardCommands();
