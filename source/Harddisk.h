4d0336401732208544dcd1758e9fdcdb0ba53393
Nick Westgate <nick.westgate@monitorbm.com>
2021-05-04 21:35:20 +1000

    InsertDisk and HD_Insert refactored to take an optional ImageInfo*

--- source/Harddisk.h
+++ source/Harddisk.h
@@ -44,7 +44,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 	void HD_Reset(void);
 	void HD_Load_Rom(const LPBYTE pCxRomPeripheral, const UINT uSlot);
 	bool HD_Select(const int iDrive);
-	BOOL HD_Insert(const int iDrive, const std::string & pszImageFilename);
+	BOOL HD_Insert(const int iDrive, const std::string & pszImageFilename, ImageInfo* pImageInfo = NULL);
 	void HD_Unplug(const int iDrive);
 	bool HD_IsDriveUnplugged(const int iDrive);
 	void HD_LoadLastDiskImage(const int iDrive);
