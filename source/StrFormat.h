0e6fc2f6e003d1829058dd7620acd323582cd233
Kelvin Lee <kiyolee@gmail.com>
2022-03-14 19:07:09 +1100

    Add ByteToHexStr() and WordToHexStr()
    - Simplify common StrFormat(), especially in Debugger (changes
      upcoming)
    - Add helpers StrAppendByteAsHex() and StrAppendWordAsHex()
    - Add helpers StrBufferAppendByteAsHex() and StrBufferAppendWordAsHex()
      for plain string buffer

--- source/StrFormat.h
+++ source/StrFormat.h
@@ -6,6 +6,7 @@
 #if defined(_MSC_VER) && _MSC_VER < 1600
 #include <basetsd.h>
 typedef UINT8 uint8_t;
+typedef UINT16 uint16_t;
 #else
 #include <cstdint>
 #endif
@@ -19,10 +20,59 @@ typedef UINT8 uint8_t;
 std::string StrFormat(const char* format, ...) ATTRIBUTE_FORMAT_PRINTF(1, 2);
 std::string StrFormatV(const char* format, va_list va);
 
+namespace {
+
+const char g_aHexDigits[16] = {
+	'0', '1', '2', '3', '4', '5', '6', '7',
+	'8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+};
+
+// No buffer overflow check or null termination. Use with caution.
+inline char* StrBufferAppendByteAsHex(char* cp, uint8_t n)
+{
+	*cp++ = g_aHexDigits[(n >> 4) & 0x0f];
+	*cp++ = g_aHexDigits[(n >> 0) & 0x0f];
+	return cp;
+}
+
+// No buffer overflow check or null termination. Use with caution.
+inline char* StrBufferAppendWordAsHex(char* cp, uint16_t n)
+{
+	*cp++ = g_aHexDigits[(n >> 12) & 0x0f];
+	*cp++ = g_aHexDigits[(n >>  8) & 0x0f];
+	*cp++ = g_aHexDigits[(n >>  4) & 0x0f];
+	*cp++ = g_aHexDigits[(n >>  0) & 0x0f];
+	return cp;
+}
+
 inline std::string& StrAppendByteAsHex(std::string& s, uint8_t n)
 {
-	const char szHex[] = "0123456789ABCDEF";
-	s += szHex[(n >> 4) & 0x0f];
-	s += szHex[n & 0x0f];
+	const char hex[2] = { g_aHexDigits[(n >> 4) & 0x0f],
+						  g_aHexDigits[(n >> 0) & 0x0f] };
+	return s.append(hex, 2);
+}
+
+inline std::string& StrAppendWordAsHex(std::string& s, uint16_t n)
+{
+	const char hex[4] = { g_aHexDigits[(n >> 12) & 0x0f],
+						  g_aHexDigits[(n >>  8) & 0x0f],
+						  g_aHexDigits[(n >>  4) & 0x0f],
+						  g_aHexDigits[(n >>  0) & 0x0f] };
+	return s.append(hex, 4);
+}
+
+inline std::string ByteToHexStr(uint8_t n)
+{
+	std::string s;
+	StrAppendByteAsHex(s, n);
 	return s;
 }
+
+inline std::string WordToHexStr(uint16_t n)
+{
+	std::string s;
+	StrAppendWordAsHex(s, n);
+	return s;
+}
+
+} // namespace
