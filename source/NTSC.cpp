259472a877be4373396f4b75680d4d3cf83f6147
tomcw <tomcw@users.noreply.github.com>
2022-10-02 21:52:13 +0100

    Fix for Rainbow demo for 50Hz/PAL Color Monitor. (fixes #1131)

--- source/NTSC.cpp
+++ source/NTSC.cpp
@@ -2038,7 +2038,8 @@ void NTSC_SetVideoMode( uint32_t uVideoModeFlags, bool bDelay/*=false*/ )
 		}
 		else
 		{
-			g_nColorBurstPixels = 1024;		// (For mid-line video mode change)
+			if (!g_nVideoMixed || g_nVideoClockVert < VIDEO_SCANNER_Y_MIXED)	// 50HZ(PAL) will kill color-burst if 'mixed and >=160' - so don't re-enable color-burst! (GH#1131)
+				g_nColorBurstPixels = 1024;		// (For mid-line video mode change)
 
 			// Switching mid-line from TEXT to graphics
 			if (GetVideo().GetVideoType() == VT_COLOR_MONITOR_NTSC &&
