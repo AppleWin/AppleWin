bb05e9a92de991c0f8ee1f8a81b9fce9f7a12dfd
Andrea Odetti <mariofutire@gmail.com>
2023-02-11 17:18:04 +0000

    Avoid a couple of warnings.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- source/Debugger/Debugger_Symbols.cpp
+++ source/Debugger/Debugger_Symbols.cpp
@@ -542,7 +542,7 @@ int ParseSymbolTable(const std::string & pPathFileName, SymbolTable_Index_e eSym
 {
 	bool bFileDisplayed = false;
 
-	const int nMaxLen = MIN(DISASM_DISPLAY_MAX_TARGET_LEN, MAX_SYMBOLS_LEN);
+	const int nMaxLen = std::min<int>(DISASM_DISPLAY_MAX_TARGET_LEN, MAX_SYMBOLS_LEN);
 
 	int nSymbolsLoaded = 0;
 
