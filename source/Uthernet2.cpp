e41059e57f46395518a08c39b8981225ffa0d2e2
sh95014 <95387068+sh95014@users.noreply.github.com>
2023-05-29 12:04:37 -0700

    fix Xcode build issues
    - explicit #includes
    - fix include path

--- source/Uthernet2.cpp
+++ source/Uthernet2.cpp
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Tfe/IPRaw.h"
 #include "Tfe/DNS.h"
 #include "W5100.h"
-#include "../Registry.h"
+#include "Registry.h"
 
 
 // Virtual DNS
