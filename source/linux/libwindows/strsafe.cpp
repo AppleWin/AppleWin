399e62b27ed0f007ca0182701f2bbe5d4fcb494e
Andrea Odetti <mariofutire@gmail.com>
2023-06-19 15:16:40 +0100

    Remove StringCbCat as no longer used.
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- source/linux/libwindows/strsafe.cpp
+++ source/linux/libwindows/strsafe.cpp
@@ -7,10 +7,3 @@ HRESULT StringCbCopy(char * pszDest, const size_t cbDest, const char * pszSrc)
   pszDest[cbDest - 1] = '\0';
   return 0;
 }
-
-HRESULT StringCbCat(char * pszDest, const size_t cbDest, const char * pszSrc)
-{
-  strncat(pszDest, pszSrc, cbDest - strlen(pszDest) - 1);
-  pszDest[cbDest - 1] = '\0';
-  return 0;
-}
--- source/linux/libwindows/strsafe.h
+++ source/linux/libwindows/strsafe.h
@@ -4,4 +4,3 @@
 #include <cstddef>
 
 HRESULT StringCbCopy(char * pszDest, const size_t cbDest, const char * pszSrc);
-HRESULT StringCbCat(char * pszDest,const size_t cbDest, const char * pszSrc);
