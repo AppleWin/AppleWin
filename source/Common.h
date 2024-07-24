eb59c90b78223c0a0e0f07b50e48706b1a9fd144
tomcw <tomcw@users.noreply.github.com>
2024-07-24 22:03:22 +0100

    Remove old REGVALUE defines

--- source/Common.h
+++ source/Common.h
@@ -122,8 +122,6 @@ enum AppMode_e
 #define  REGVALUE_LAST_DISK_1		"Last Disk Image 1"
 #define  REGVALUE_LAST_DISK_2		"Last Disk Image 2"
 #define  REGVALUE_LAST_HARDDISK_	"Last Harddisk Image "
-#define  REGVALUE_LAST_HARDDISK_1	"Last Harddisk Image 1"
-#define  REGVALUE_LAST_HARDDISK_2	"Last Harddisk Image 2"
 
 // Preferences 
 #define REG_PREFS						"Preferences"
