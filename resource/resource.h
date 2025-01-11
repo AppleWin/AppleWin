fcd3f6d6d804afa7ddbef8c4eafcce34d6b81ebe
michaelangel007 <michaelangel007@sharedcraft.com>
2025-01-11 11:46:47 -0800

    #1364 Remove hard-coded boot sector and use firmware/BootSector/bootsector.bin resource instead

--- resource/resource.h
+++ resource/resource.h
@@ -39,9 +39,10 @@
 #define IDR_MOCKINGBOARD_D_FW           135
 #define IDR_MOUSEINTERFACE_FW           136
 #define IDR_THUNDERCLOCKPLUS_FW         137
-#define IDR_FREEZES_F8_ROM              138
-#define IDR_PRAVETS_82_ROM              139
-#define IDR_PRAVETS_8M_ROM              140
+#define IDR_BOOT_SECTOR                 138
+#define IDR_FREEZES_F8_ROM              139
+#define IDR_PRAVETS_82_ROM              140
+#define IDR_PRAVETS_8M_ROM              141
 #define IDR_PRAVETS_8C_ROM              142
 #define IDR_MENU_DISK_POPUP             143
 #define IDD_ABOUT                       145
