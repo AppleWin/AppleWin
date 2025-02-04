961962d5f9953c67d1806911e726d0fa83642815
michaelangel007 <michaelangel007@sharedcraft.com>
2025-02-04 11:23:48 -0800

    Add BITSY.BOOT and QUIT.SYSTEM

--- resource/resource.h
+++ resource/resource.h
@@ -64,7 +64,9 @@
 #define IDR_OS_DOS33                    161
 #define IDR_OS_PRODOS243                162
 #define IDR_BOOT_SECTOR_PRODOS243       163
-#define IDR_FILE_BASIC17                164
+#define IDR_FILE_BITSY_BOOT             164
+#define IDR_FILE_BITSY_BYE              165
+#define IDR_FILE_BASIC17                166
 #define IDC_KEYB_BUFFER_ENABLE          1005
 #define IDC_SAVESTATE                   1006
 #define IDC_SAVESTATE_ON_EXIT           1007
@@ -163,7 +165,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        165
+#define _APS_NEXT_RESOURCE_VALUE        167
 #define _APS_NEXT_COMMAND_VALUE         40022
 #define _APS_NEXT_CONTROL_VALUE         1093
 #define _APS_NEXT_SYMED_VALUE           101
