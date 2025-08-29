61e801a3cd9f746ea7eb3f47e36376bd90d85c2e
michaelangel007 <michaelangel007@sharedcraft.com>
2025-08-29 08:37:48 -0700

    Always display the VERSION (to match MOTD)

--- source/Debugger/Debugger_Help.cpp
+++ source/Debugger/Debugger_Help.cpp
@@ -1459,16 +1459,18 @@ Update_t CmdVersion (int nArgs)
 	int nFixMinor;
 	UnpackVersion( nVersion, nMajor, nMinor, nFixMajor, nFixMinor );
 
-	ConsolePrintFormat("  Emulator:  %s%s%s (%d-bit build)    Debugger: %s%d.%d.%d.%d%s"
+	// Use ConsolePrint() like CmdMOTD does so the version is always displayed
+	std::string sText = StrFormat("  Emulator:  %s%s%s (%d-bit build)    Debugger: %s%d.%d.%d.%d%s"
 		, CHC_SYMBOL
 		, g_VERSIONSTRING.c_str()
 		, CHC_DEFAULT
 		, GetCompilationTarget()
-		//
+
 		, CHC_SYMBOL
 		, nMajor, nMinor, nFixMajor, nFixMinor
 		, CHC_DEFAULT
 	);
+	ConsolePrint( sText.c_str() );
 
 	if (nArgs)
 	{
