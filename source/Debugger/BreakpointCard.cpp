30fb1f381a86bc8c1c4ffcd07a187a7ab5745f2d
tomcw <tomcw@users.noreply.github.com>
2025-10-18 15:39:11 +0100

    Breakpoint card: add save/load state support

--- source/Debugger/BreakpointCard.cpp
+++ source/Debugger/BreakpointCard.cpp
@@ -70,6 +70,7 @@
 #include "Debug.h"
 #include "BreakpointCard.h"
 #include "Memory.h"
+#include "YamlHelper.h"
 
 BreakpointCard::BreakpointCard(UINT slot) :
 	Card(CT_BreakpointCard, slot),
@@ -234,4 +235,30 @@ void BreakpointCard::Deferred(uint8_t type, uint16_t addrStart, uint16_t addrEnd
 		break;
 	}
 
+}
+
+//===========================================================================
+
+static const UINT kUNIT_VERSION = 1;
+
+const std::string& BreakpointCard::GetSnapshotCardName(void)
+{
+	static const std::string name("Breakpoint");
+	return name;
+}
+
+void BreakpointCard::SaveSnapshot(YamlSaveHelper& yamlSaveHelper)
+{
+	YamlSaveHelper::Slot slot(yamlSaveHelper, GetSnapshotCardName(), m_slot, kUNIT_VERSION);
+
+	YamlSaveHelper::Label unit(yamlSaveHelper, "%s: null\n", SS_YAML_KEY_STATE);
+	// NB. No state for this card
+}
+
+bool BreakpointCard::LoadSnapshot(YamlLoadHelper& yamlLoadHelper, UINT version)
+{
+	if (version < 1 || version > kUNIT_VERSION)
+		ThrowErrorInvalidVersion(version);
+
+	return true;
 }
